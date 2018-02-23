960a3ad9fc8918532c9eaacb4dca2bc8dee0cfac
Nathan Lamothe <e1436741@cmaisonneuve.qc.ca>
2018-02-23 14:32:18 -0500

    Ajout de la page profil

--- tp1.js
+++ tp1.js
@@ -36,6 +36,14 @@ app.get('/membres', function (req, res) {
  }) 
 })
 
+app.get('/profil', function (req, res) {
+   let cursor = db.collection('adresse').find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ // transfert du contenu vers la vue adresses.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('profil.ejs', {adresses: resultat})
+ }) 
+})
 
 app.get('/detruire/:id', (req, res) => {
  var id = req.params.id 
